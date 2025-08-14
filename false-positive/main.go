597b4983b5eec5785b6085476122dd0875f019e6
goatwu1993 <peteeelol@gmail.com>
2025-08-14 12:32:01 +0800

    chore: change package to vault sdk

--- false-positive/main.go
+++ false-positive/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/hashicorp/vault/api/auth/kubernetes"
+	"github.com/hashicorp/vault/sdk/helper/errutil"
 )
 
 func main() {
-	_, err := kubernetes.NewKubernetesAuth("customrole")
+	var err error
+	err = &errutil.InternalError{
+		Err: "This is an internal error",
+	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error creating KubernetesAuth: %v\n", err)
 		os.Exit(1)
